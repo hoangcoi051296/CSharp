0beb9602065d61b7ed87c2652570eeb7758a23e5
hoangcoi051296 <49927540+hoangcoi051296@users.noreply.github.com>
2019-12-27 16:57:26 +0700

    Lab1

---
+++ T1904A/T1904A/Fibonacy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace T1904A
+{
+    public class Fibonacy
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nhap n");
+            int n = Convert.ToInt32(Console.ReadLine());
+          Console.WriteLine();
+          for (int i = 1; i < 100; i++)
+          {
+              if (n > Fibonacci(i) & n < Fibonacci(i + 1))
+              {
+                  Console.WriteLine(Fibonacci(i));
+              }
+          }
+          
+        }
+        public static int Fibonacci(int n)
+        {
+            if (n == 1 || n == 2)
+                return 1;
+            return Fibonacci(n - 1) + Fibonacci(n - 2);
+        }
+    }
+}
