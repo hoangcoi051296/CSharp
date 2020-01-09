136c1473a545fcd881a6ebcd5d36a6ff21b0c2e5
hoangcoi051296 <49927540+hoangcoi051296@users.noreply.github.com>
2020-01-10 00:43:22 +0700

    Assigment4

---
+++ T1904A/T1904A/Assigment4.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Threading;
+
+namespace T1904A
+
+{
+    public class Assigment4
+    {
+        private static int n;
+        static void Main(string[] args)
+        {    Console.WriteLine("Nhap n"); 
+            n = Convert.ToInt32(Console.ReadLine());
+            Thread t2 =new Thread(Ooo);
+            t2.Start();
+            Thread t1 =new Thread(hhh);
+            t1.Start();
+            
+          
+        }
+
+        public static void hhh()
+        {  
+            try
+            {
+                for (int i = 1;; i++)
+                {
+                    if (Fibonacci(i) < n)
+                    {
+                        Thread.Sleep(100);
+                    }
+                    if (n > Fibonacci(i) & n < Fibonacci(i + 1))
+                    {
+                       
+
+                        Console.WriteLine(Fibonacci(i));
+
+                    }
+                }
+
+            }catch (Exception e) { }
+            static int Fibonacci(int n)
+            {
+                if (n == 1 || n == 2)
+                    return 1;
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+                
+            }
+        
+            
+        }
+        public static void Ooo()
+        {
+            int a = 0;
+            try
+            {
+                for (int i = 1;; i++)
+                {
+                    if (Fibonacci(i) < n)
+                    {
+                        Thread.Sleep(100);
+                        a=a+100;
+                        Console.WriteLine(a+"ms");
+                    }
+                    
+                }
+                        
+                   
+                
+               
+                
+            }catch (Exception e) { }
+            static int Fibonacci(int n)
+            {
+                if (n == 1 || n == 2)
+                    return 1;
+                return Fibonacci(n - 1) + Fibonacci(n - 2);
+            }
+        
+            
+        }
+        
+    }
+}
