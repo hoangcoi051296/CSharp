e27e2de7d4de0fefc789eb4cdb15c3e114144eb3
hoangcoi051296 <49927540+hoangcoi051296@users.noreply.github.com>
2020-01-13 08:34:51 +0700

    Assigment5

---
+++ T1904A/T1904A/Customer.cs
@@ -0,0 +1,171 @@
+﻿using System;
+
+namespace T1904A
+{
+    public class Customer
+    {
+        public int ID;
+        public string ten;
+        public string ngayhoadon;
+        public float sodien;
+        public int[] dongia = {1000,1200,1500,2000};
+        public static int[] dinhmuc = {0,50,100,200};
+        public float tongtien;
+
+        public Customer() { }
+
+        public Customer(int id, string ten, string ngayhoadon, float sodien, float tongtien)
+        {
+            ID = id;
+            this.ten = ten;
+            this.ngayhoadon = ngayhoadon;
+            this.sodien = sodien;
+            this.tongtien = tongtien;
+        }
+
+        public int Id
+        {
+            get => ID;
+            set => ID = value;
+        }
+
+        public string Ten
+        {
+            get => ten;
+            set => ten = value;
+        }
+
+        public string Ngayhoadon
+        {
+            get => ngayhoadon;
+            set => ngayhoadon = value;
+        }
+
+        public float Sodien
+        {
+            get => sodien;
+            set => sodien = value;
+        }
+        
+        
+        public void InputCustomer(){
+            Console.WriteLine("Nhap thong tin khach hang: ");
+            Console.WriteLine("Nhap ID khach hang: ");
+            Id =Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nhap ten khach hang: ");
+            ten = Console.ReadLine();
+            Console.WriteLine("Nhap ngay in hoa don: ");
+            ngayhoadon = Console.ReadLine();
+            Console.WriteLine("Nhap so luong dien tieu thu (kW): ");
+            sodien= Id =Convert.ToInt32(Console.ReadLine());
+        }
+        
+        public void ShowCustomer(){
+            Console.WriteLine("ID: "+ID+" - Ten KH: "+ten
+                              +" - Ngay in hoa don: "+ngayhoadon+" - So luong dien tieu thu (kW): "+sodien);
+        }
+        
+        public void tinhtongtien(){
+            if(sodien<50){
+                tongtien = sodien * dongia[0];
+            }
+            else if(sodien>50& sodien<100){
+                tongtien = 50*dongia[0] + (sodien-50)*dongia[1];
+            }
+            else if(sodien>100& sodien<200){
+                tongtien = 50*dongia[0] + 50*dongia[1] + ( sodien-100)*dongia[2];
+            }
+            else {
+                tongtien = 50*dongia[0] + 50*dongia[1] + 100*dongia[2] + (sodien-200)*dongia[3];
+            }
+        }
+    }
+
+    
+    
+    public class CustomerVN : Customer
+    {
+        private String doituong;
+
+        public string Doituong
+        {
+            get => doituong;
+            set => doituong = value;
+        }
+
+        public CustomerVN( )
+        {
+          InputCustomer();
+          this.tinhtongtien();
+          
+        }
+
+        public CustomerVN(int id, string ten, string ngayhoadon, float sodien, float tongtien, string doituong) : base(id, ten, ngayhoadon, sodien, tongtien)
+        {
+            this.doituong = doituong;
+            this.tinhtongtien();
+        }
+
+        public void InputCustomer()
+        {
+            base.InputCustomer();
+            Console.WriteLine("Nhap doi tuong");
+            doituong = Console.ReadLine();
+
+        }
+
+        public void ShowCustomer()
+        {
+            base.ShowCustomer();
+           Console.WriteLine("Doi tuong: "+doituong+" - Tong tien: "+this.tongtien);
+        }
+        
+        public void tinhtongtien() {
+            base.tinhtongtien();
+        }
+        
+    }
+
+    
+    
+    
+    public class CustomerNN : Customer
+    {
+        private String quoctich;
+
+        public string Quoctich
+        {
+            get => quoctich;
+            set => quoctich = value;
+        }
+
+        public CustomerNN() {
+            InputCustomer();
+            this.tinhtongtien();
+        }
+
+        public CustomerNN(int id, string ten, string ngayhoadon, float sodien, float tongtien) : base(id, ten, ngayhoadon, sodien, tongtien)
+        {
+            this.quoctich = quoctich;
+            this.tinhtongtien();
+        }
+        public void InputCustomer()
+        {
+            base.InputCustomer();
+            Console.WriteLine("Nhap quoc tich : ");
+            quoctich = Console.ReadLine();
+
+        }
+
+        public void ShowCustomer()
+        {
+            base.ShowCustomer();
+            Console.WriteLine("Quoc tich: "+quoctich+" - Tong tien: "+this.tongtien);
+        }
+        
+        public void tinhtongtien() {
+            tongtien = this.sodien * this.dongia[3];
+        }
+
+    }
+}
---
+++ T1904A/T1904A/ListCustomer.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace T1904A
+{
+    public class ListCustomer
+    {
+        public List<CustomerVN> listVN;
+        public List<CustomerNN> listNN;
+
+        public ListCustomer()
+        {
+            listVN=new List<CustomerVN>();
+            listNN=new List<CustomerNN>();
+        }
+        public void addCusVN() {
+            Console.WriteLine("Them khach hang Viet Nam vao danh sach");
+            CustomerVN newCustomer = new CustomerVN();
+            listVN.Add(newCustomer);
+        }
+        public void addCusNN() {
+            Console.WriteLine("Them khach hang nuoc ngoai vao danh sach");
+            CustomerNN newCustomer = new CustomerNN();
+            listNN.Add(newCustomer);
+        }
+        public void showlistCustomer(){
+            Console.WriteLine("Danh sach cac khach hang Viet Nam");
+            foreach (var a  in listVN)
+            {
+                a.ShowCustomer();
+            }
+            Console.WriteLine("Danh sach cac khach hang Nuoc ngoai");
+            foreach (var b  in listNN)
+            {
+                b.ShowCustomer();
+            }
+           
+        }
+
+        public void countCus(){
+           Console.WriteLine("Hien tai co "+(listVN.Count+listNN.Count)+" khach hang: "
+                               +listVN.Count+" khach hang VN, "+listNN.Count+" khach hang nuoc ngoai");
+        }
+
+        public void tienTBnuocngoai(){
+            float temp = 0;
+            foreach (var c in listNN)
+            {
+                temp = temp + c.tongtien;
+            }
+            Console.WriteLine("Trung binh tien dien nguoi nuoc ngoai: "+temp/listNN.Count);
+        }
+    }
+}
