a66a3083ccf8f58fa5b9c2c29fe227119e6135d7
hoangcoi051296 <49927540+hoangcoi051296@users.noreply.github.com>
2020-03-02 16:02:25 +0700

    Practical

---
+++ T1904A/T1904A/Delegate.cs
@@ -0,0 +1,37 @@
+﻿using System.Threading;
+
+namespace T1904A
+{
+    public delegate void PrimeNumberFinder(int n);
+    public class Delegate
+    {
+        public event PrimeNumberFinder OnePrimeNumber;
+
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                if (isPrime(n)) {
+                   
+                } 
+            }
+            
+            OnePrimeNumber o =new OnePrimeNumber();
+            
+        }
+        
+     
+        public static boolean isPrime(int n) {
+            if (n <= 1) {
+                return false;
+            }
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+    
+}
---
+++ T1904A/T1904A/MobilePhone.cs
@@ -0,0 +1,19 @@
+﻿namespace T1904A
+{
+    public class MobilePhone : Phone
+    {
+        public MobilePhone()
+        {
+        }
+
+        public MobilePhone(string phonename, string phonetype, float phoneprice) : base(phonename, phonetype, phoneprice)
+        {
+        }
+
+        static void Main(string[] args)
+        {
+            Phone p =new Phone();
+            p.display();
+        }
+    }
+}
